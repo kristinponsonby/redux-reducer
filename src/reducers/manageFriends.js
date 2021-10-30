90bef9a3b68bcf2afd728c529f2e24ba5ebe0dbb
Kristin <kristinponsonby@gmail.com>
2021-10-30 10:38:57 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
 export function manageFriends(state, action){
+    swtich (action.type) {
+        case "ADD_FRIEND":
+            return { friends: state.friends + 1 };
+        case "REMOVE_FRIEND":
+            return { friends: state.friends - 1 };
+            default: 
+              return state
+    }
 }
+    
+    
+
+let state = { friends: [] };
+let action = { type: "ADD_FRIEND" }
+
+managePresents(state, action);
